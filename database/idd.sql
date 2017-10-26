b744bb02b3680028a390b73c943e694f2a17755d
juan.jusue <juan.jusue@UTWKS0508.liveu-tad.es>
2017-10-26 11:04:20 +0200

    se nos habia olvidado subir el .sql

--- database/idd.sql
+++ database/idd.sql
@@ -1,13 +1,15 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 26-09-2017 a las 17:55:26
--- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 5.6.28
+-- Host: 127.0.0.1
+-- Generation Time: Oct 26, 2017 at 11:02 AM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,13 +19,34 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `idd`
+-- Database: `idd`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `jugadores`
+-- Table structure for table `equipos`
+--
+
+CREATE TABLE `equipos` (
+  `ID` int(5) NOT NULL,
+  `nombre` varchar(250) NOT NULL,
+  `Descripcion` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `equipos`
+--
+
+INSERT INTO `equipos` (`ID`, `nombre`, `Descripcion`) VALUES
+(1, 'Real Madrid', 'El mejor equipo '),
+(2, 'Barcelona', 'El peor equipo'),
+(3, 'Oporto', 'Un equipo sin mas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jugadores`
 --
 
 CREATE TABLE `jugadores` (
@@ -31,38 +54,61 @@ CREATE TABLE `jugadores` (
   `Nombre` varchar(80) NOT NULL,
   `Apellido` varchar(80) NOT NULL,
   `Posicion` varchar(80) NOT NULL,
-  `Equipo` varchar(80) NOT NULL
+  `ID_Equipo` int(80) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Volcado de datos para la tabla `jugadores`
+-- Dumping data for table `jugadores`
 --
 
-INSERT INTO `jugadores` (`ID`, `Nombre`, `Apellido`, `Posicion`, `Equipo`) VALUES
-(1, 'Cristiano', 'Ronaldo', 'Delantero', 'Real Madrid'),
-(3, 'Leonardo', 'Messi', 'Delantero', 'Barcelona'),
-(4, 'Iker', 'Casillas', 'Portero', 'Oporto'),
-(5, 'Carles', 'Puyol', 'Defensa', 'Barcelona');
+INSERT INTO `jugadores` (`ID`, `Nombre`, `Apellido`, `Posicion`, `ID_Equipo`) VALUES
+(6, 'Iker', 'Casillas', 'Portero', 3),
+(7, 'Cristiano', 'Ronaldo', 'Delantero', 1),
+(8, 'Leonardo', 'Messi', 'Delantero', 2);
 
 --
--- Índices para tablas volcadas
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `jugadores`
+-- Indexes for table `equipos`
 --
-ALTER TABLE `jugadores`
+ALTER TABLE `equipos`
   ADD PRIMARY KEY (`ID`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indexes for table `jugadores`
+--
+ALTER TABLE `jugadores`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `Equipo` (`ID_Equipo`),
+  ADD KEY `ID_Equipo` (`ID_Equipo`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `jugadores`
+-- AUTO_INCREMENT for table `equipos`
+--
+ALTER TABLE `equipos`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `jugadores`
 --
 ALTER TABLE `jugadores`
-  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `ID` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `equipos`
+--
+ALTER TABLE `equipos`
+  ADD CONSTRAINT `equipos_ibfk_1` FOREIGN KEY (`ID`) REFERENCES `jugadores` (`ID_Equipo`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
